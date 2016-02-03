d495ebfb712cd69c0829f22b7cfa7bb5b144605b
huajunsir <huajunsir@gmail.com>
2016-02-03 10:45:44 +0800

    更新了日期处理

--- src/cn/edu/zju/finbase/SegmenterDIR.java
+++ src/cn/edu/zju/finbase/SegmenterDIR.java
@@ -21,7 +21,7 @@ public class SegmenterDIR {
 	private String inputDir=""; //分词的文本目录
 	private String basedir = "./nlp-tool/stanford-segmenter-2015-12-09/data";
 	private String article_type="";
-	private String publish_date="2015-01-01";
+	//private String publish_date="2015-01-01";
 	private boolean init_table=false;
 	
 	int max_text_length=300000; //控制文件大小
@@ -67,7 +67,8 @@ public class SegmenterDIR {
 					  "text text," +
 		  	          "file_name text," +
 					  "article_type text," + 
-		  	          "publish_date text" +
+		  	          "publish_date text," +
+					  "publish_year text" +
 					  ")";
 				st.executeUpdate(sql);
 				st.close();
@@ -117,35 +118,83 @@ public class SegmenterDIR {
 	}
 	
 	
-	public String getPublishDate(String article_content) throws IOException{
-
-		  String publish_date="";
-		  File dir = new File(this.inputDir);
-		  File[] files = dir.listFiles();
-		  for(int i=1;i<files.length;i++){
-			  System.out.println("-对文件" + files[i].toString() + "-进行分词-文件大小:"+files[i].length()+"已处理"+i+"个文件--");       
-				
-			  if(files[i].length() > max_text_length) continue; //不处理过大的文本文件。
-			  java.nio.charset.Charset charset = detector.detectCodepage(files[i].toURL());// 检测文本的编码格式，可能是gb2312, window-1252等。	
+	public String[] getPublishDate(String article_content) throws IOException{
+		    String publish_date=null;
+		    String publish_year=null;
 			  
-			  String encoding=charset.toString();
-			  if(encoding=="windows-1252") encoding="utf-16le"; //检测到的windows-1252编码实际上是utf-16le编码
-    	    
-			  article_content = IOUtils.slurpFile(files[i].toString(),encoding); //基于给定的编码读取文本内容。
-	
-			  Pattern pattern = Pattern.compile("\\d{4}\\s{1,3}年\\s{1,3}\\d{1,2}\\s{1,3}月\\s{1,3}\\d{1,2}\\s{1,3}日");
+		    //匹配年月日，以最后出现的年月日为准
+			  Pattern pattern = Pattern.compile("[\\d\\s零O0○〇一二三四五六七八九]{4,8}\\s{0,3}年\\s{0,3}[\\d零0O〇元正○一二三四五六七八九]{1,2}\\s{0,3}月\\s{0,3}[\\d零0O〇初○一二三四五六七八九]{1,2}\\s{0,3}日");
 			  Matcher matcher = pattern.matcher(article_content);
-			  if(matcher.find())
-				  System.out.println(matcher.group());
-		 
-		  		}
-		//  Pattern pattern = Pattern.compile("href=\"(.+?)\"");
-		 // Matcher matcher = pattern.matcher("<a href=\"index.html\">主页</a>");
-		 // if(matcher.find())
-		 //   System.out.println(matcher.group(0));
-
-		  return publish_date;
-		
+			  while(matcher.find()) {
+			    	publish_date=matcher.group();
+			  }
+			  
+	
+			  //匹配年
+			  pattern = Pattern.compile("([\\d零O○〇一二三四五六七八九]{4,8}\\s{0,3}年)|(20[\\d]{2})");
+			  matcher = pattern.matcher(article_content);
+			  while(matcher.find()) {
+			    	publish_year=matcher.group();
+			  }
+				
+			  if(publish_date==null){
+				  publish_date=publish_year;
+				  
+			  }
+			  else{
+				    publish_date=publish_date.trim().replaceAll(" ", "");
+				    publish_date=publish_date.replaceAll("[年月]", "-");
+				    publish_date=publish_date.replaceAll("日", "");
+				    publish_date=publish_date.replaceAll("[零O○〇]", "0");
+					publish_date=publish_date.replaceAll("一", "1");
+					publish_date=publish_date.replaceAll("二", "2");
+					publish_date=publish_date.replaceAll("三", "3");
+					publish_date=publish_date.replaceAll("四", "4");
+					publish_date=publish_date.replaceAll("五", "5");
+					publish_date=publish_date.replaceAll("六", "6");
+					publish_date=publish_date.replaceAll("七", "7");
+					publish_date=publish_date.replaceAll("八", "8");
+					publish_date=publish_date.replaceAll("九", "9");
+				 
+					publish_year=publish_date.substring(0, 4);
+			  }
+				
+			  if(publish_year==null){
+					publish_date=publish_year="3000";//没有找到匹配，则设置为3000.
+	
+			  }
+			
+				
+			  publish_date=publish_date.trim().replaceAll(" ", "");
+			    publish_date=publish_date.replaceAll("[年月]", "-");
+			    publish_date=publish_date.replaceAll("日", "");
+			    publish_date=publish_date.replaceAll("[零O○〇]", "0");
+				publish_date=publish_date.replaceAll("一", "1");
+				publish_date=publish_date.replaceAll("二", "2");
+				publish_date=publish_date.replaceAll("三", "3");
+				publish_date=publish_date.replaceAll("四", "4");
+				publish_date=publish_date.replaceAll("五", "5");
+				publish_date=publish_date.replaceAll("六", "6");
+				publish_date=publish_date.replaceAll("七", "7");
+				publish_date=publish_date.replaceAll("八", "8");
+				publish_date=publish_date.replaceAll("九", "9");
+	
+				publish_year=publish_year.trim().replaceAll(" ", "");
+				publish_year=publish_year.replaceAll("年", "");
+				publish_year=publish_year.replaceAll("[零O○〇]", "0");
+				publish_year=publish_year.replaceAll("一", "1");
+				publish_year=publish_year.replaceAll("二", "2");
+				publish_year=publish_year.replaceAll("三", "3");
+				publish_year=publish_year.replaceAll("四", "4");
+				publish_year=publish_year.replaceAll("五", "5");
+				publish_year=publish_year.replaceAll("六", "6");
+				publish_year=publish_year.replaceAll("七", "7");
+				publish_year=publish_year.replaceAll("八", "8");
+				publish_year=publish_year.replaceAll("九", "9");
+	
+			
+			String[] tmp= {publish_date, publish_year};
+			return tmp;	
 	}
 	
 	public void segmentDir() {
@@ -191,14 +240,17 @@ public class SegmenterDIR {
 					        List<String> segmented=segmenter.segmentString(fileContents);
 					        Iterator<String> it=segmented.iterator();
 					        String temp=" ";
+					        
+					        String[] publish_time=this.getPublishDate(fileContents);
+					        
 					        while(it.hasNext()) temp += (String) it.next() +" "; 
 					        //分词结果写入数据库 
 					        
 					        Statement st = con.createStatement();
 						    
 							//最后插入数据库
-							String sql = "insert into articles(text, file_name, article_type, publish_date) values('"+ 
-										temp + "','"+ files[i].getName() +"','"+ article_type+"','"+ publish_date +"')";
+							String sql = "insert into articles(text, file_name, article_type, publish_date,publish_year) values('"+ 
+										temp + "','"+ files[i].getName() +"','"+ article_type+"','"+ publish_time[0]+"','"+ publish_time[1] +"')";
 							
 							//System.out.println(sql);
 							st.executeUpdate(sql);
@@ -306,5 +358,4 @@ public class SegmenterDIR {
 	public void setMax_text_length(int max_text_length) {
 		this.max_text_length = max_text_length;
 	}
-
 }
--- src/demo/TestClass.java
+++ src/demo/TestClass.java
@@ -1,13 +1,20 @@
 package demo;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat; 
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import edu.stanford.nlp.io.IOUtils;
 import info.monitorenter.cpdetector.io.*;
 
 public class TestClass {
@@ -91,8 +98,7 @@ public class TestClass {
 		 // System.out.println(Charset.availableCharsets().keySet());
 	}
 
-	public static void main(String[] args) {
-		
+	public void testRemoteConnection(){
 		File file=new File("./db.url");
         //读取每个文件内容
         InputStreamReader read;
@@ -130,4 +136,129 @@ public class TestClass {
 
 	}
 
+	public void testGetPublishDate() throws MalformedURLException, IOException{
+		  String publish_date=null;
+		  String publish_year=null;
+		  File dir = new File("./inputtxt");
+		  File[] files = dir.listFiles();
+		  
+		  CodepageDetectorProxy detector = CodepageDetectorProxy.getInstance(); // A singleton.
+		 //初始化中文字符编码格式探测器。
+	    	detector.add(JChardetFacade.getInstance());
+	    	detector.add(ASCIIDetector.getInstance());
+	    	
+	    	int count=0;
+	    	
+	    	StringBuffer sb=new StringBuffer();
+		  for(int i=1;i<files.length;i++){
+			  publish_date=null;
+			  publish_year=null;
+			  System.out.println("-对文件" + files[i].toString() + "-进行分词-文件大小:"+files[i].length()+"已处理"+i+"个文件--");       
+				
+		      java.nio.charset.Charset charset = detector.detectCodepage(files[i].toURL());// 检测文本的编码格式，可能是gb2312, window-1252等。	
+			  
+			  String encoding=charset.toString();
+			  if(encoding=="windows-1252") encoding="utf-16le"; //检测到的windows-1252编码实际上是utf-16le编码
+    	      if(encoding=="void" || encoding==null)continue;
+    	      
+			  String article_content = IOUtils.slurpFile(files[i].toString(),encoding); //基于给定的编码读取文本内容。
+	
+			  //过滤空的或过小的文档
+			  if(article_content==null || article_content.trim().length()<30)continue; 
+			  
+			  //匹配年月日，以最后出现的年月日为准
+			  Pattern pattern = Pattern.compile("[\\d\\s零O0○〇一二三四五六七八九]{4,8}\\s{0,3}年\\s{0,3}[\\d零0O〇元正○一二三四五六七八九]{1,2}\\s{0,3}月\\s{0,3}[\\d零0O〇初○一二三四五六七八九]{1,2}\\s{0,3}日");
+			  Matcher matcher = pattern.matcher(article_content);
+			  while(matcher.find()) {
+			    	publish_date=matcher.group();
+			  }
+			  
+	
+			  //匹配年
+			  pattern = Pattern.compile("([\\d零O○〇一二三四五六七八九]{4,8}\\s{0,3}年)|(20[\\d]{2})");
+			  matcher = pattern.matcher(article_content);
+			  while(matcher.find()) {
+			    	publish_year=matcher.group();
+			  }
+				
+			  if(publish_date==null){
+				  publish_date=publish_year;
+				  
+			  }
+			  else{
+				    publish_date=publish_date.trim().replaceAll(" ", "");
+				    publish_date=publish_date.replaceAll("[年月]", "-");
+				    publish_date=publish_date.replaceAll("日", "");
+				    publish_date=publish_date.replaceAll("[零O○〇]", "0");
+					publish_date=publish_date.replaceAll("一", "1");
+					publish_date=publish_date.replaceAll("二", "2");
+					publish_date=publish_date.replaceAll("三", "3");
+					publish_date=publish_date.replaceAll("四", "4");
+					publish_date=publish_date.replaceAll("五", "5");
+					publish_date=publish_date.replaceAll("六", "6");
+					publish_date=publish_date.replaceAll("七", "7");
+					publish_date=publish_date.replaceAll("八", "8");
+					publish_date=publish_date.replaceAll("九", "9");
+				 
+					publish_year=publish_date.substring(0, 4);
+			  }
+				
+			  if(publish_year==null){
+					publish_date=publish_year="3000";//没有找到匹配，则设置为3000.
+					count++;
+					sb.append(files[i].toString());
+			  }
+			
+				
+			  publish_date=publish_date.trim().replaceAll(" ", "");
+			    publish_date=publish_date.replaceAll("[年月]", "-");
+			    publish_date=publish_date.replaceAll("日", "");
+			    publish_date=publish_date.replaceAll("[零O○〇]", "0");
+				publish_date=publish_date.replaceAll("一", "1");
+				publish_date=publish_date.replaceAll("二", "2");
+				publish_date=publish_date.replaceAll("三", "3");
+				publish_date=publish_date.replaceAll("四", "4");
+				publish_date=publish_date.replaceAll("五", "5");
+				publish_date=publish_date.replaceAll("六", "6");
+				publish_date=publish_date.replaceAll("七", "7");
+				publish_date=publish_date.replaceAll("八", "8");
+				publish_date=publish_date.replaceAll("九", "9");
+	
+				publish_year=publish_year.trim().replaceAll(" ", "");
+				publish_year=publish_year.replaceAll("年", "");
+				publish_year=publish_year.replaceAll("[零O○〇]", "0");
+				publish_year=publish_year.replaceAll("一", "1");
+				publish_year=publish_year.replaceAll("二", "2");
+				publish_year=publish_year.replaceAll("三", "3");
+				publish_year=publish_year.replaceAll("四", "4");
+				publish_year=publish_year.replaceAll("五", "5");
+				publish_year=publish_year.replaceAll("六", "6");
+				publish_year=publish_year.replaceAll("七", "7");
+				publish_year=publish_year.replaceAll("八", "8");
+				publish_year=publish_year.replaceAll("九", "9");
+				
+				System.out.println(publish_date+"==="+publish_year);
+				
+		  }
+		  
+		  System.out.println(sb.toString());
+		  System.out.println(count);
+		  
+
+		//  Pattern pattern = Pattern.compile("href=\"(.+?)\"");
+		 // Matcher matcher = pattern.matcher("<a href=\"index.html\">主页</a>");
+		 // if(matcher.find())
+		 //   System.out.println(matcher.group(0));
+
+	}
+	public static void main(String[] args) {
+		TestClass tc=new TestClass();
+		try {
+			tc.testGetPublishDate();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
